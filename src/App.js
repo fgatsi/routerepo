656b079925e353fcef5b3fc51fdfb231de119001
fgatsi <fgatsi@nd.edu>
2023-07-06 12:56:44 -0400

    Saved changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
 import Components from "./Components/Components.js";
+import Parse from "parse";
+import * as Env from "./environments.js";
 
 function App() {
   return <Components />;
 }
+
+export default App;
+
+
+
+
+Parse.initialize(Env.APPLICATION_ID, Env.JAVASCRIPT_KEY);
+Parse.serverURL = Env.SERVER_URL;
+
+const App = () => {
+  return <Components />;
+};
 
 export default App;
